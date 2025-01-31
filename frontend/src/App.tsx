4efef2844fb6ab8ff88bef828541b0a4c1240344
rishisulakhe <rishiprasadsulakhe@gmail.com>
2025-01-31 23:10:34 +0530

    Route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <BrowserRouter>
      <Routes>
+      <Route path='/' element={<Signin />} />
       <Route path='/signup' element={<Signup />} />
       <Route path='/signin' element={<Signin />} />
       <Route path='/blog/:id' element={<Blog />} />
